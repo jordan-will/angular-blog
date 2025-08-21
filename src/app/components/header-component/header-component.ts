308ecd0f85ee5b9080d6a36794f95236494d152a
Jordan Willian <jordan.willian.mp@gmail.com>
2025-08-21 16:14:29 -0300

    responsiveness added on view. Header componente refatored to show search icon only in pages with data to filter

--- src/app/components/header-component/header-component.ts
+++ src/app/components/header-component/header-component.ts
@@ -1,20 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit, output } from '@angular/core';
+import { RouterLink, Router, NavigationEnd } from '@angular/router';
 import { HeaderSearch } from '@components/header-search/header-search';
 import { SearchService } from '@services/search-service';
 import { UserService } from '@services/user-service';
 import { User } from 'interfaces/user';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header-component',
   imports: [
     CommonModule,
-    HeaderSearch
+    HeaderSearch,
+    RouterLink
   ],
   templateUrl: './header-component.html',
   styleUrl: './header-component.scss'
 })
 export class HeaderComponent implements OnInit {
+  private router = inject(Router)
   private userService = inject(UserService)
   private searchService = inject(SearchService)
 
@@ -22,13 +26,15 @@ export class HeaderComponent implements OnInit {
   showSearchForm: boolean = false
   isLogged: boolean = false
   sideMenu = output<'left' | 'right'>()
-
+  public showSearchBtn:boolean = false
 
   ngOnInit(): void {
     this.userService.user$.subscribe(user => {
       this.isLogged = user !== null ? true : false
       this.user = user
     })
+
+    this.listenShowSearch()
   }
 
   toggleMenu(type: 'left' | 'right') {
@@ -39,6 +45,16 @@ export class HeaderComponent implements OnInit {
   {
     this.showSearchForm = !this.showSearchForm
     if(!this.showSearchForm) this.searchService.handleSearch(null)
+  }
+
+  listenShowSearch(){
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    )
+    .subscribe((event: NavigationEnd) => {
+      const showRoutes = ['/home', '/my-stories']
+      this.showSearchBtn = showRoutes.some(route => event.urlAfterRedirects.startsWith(route))
+    })
   }
 
 }
